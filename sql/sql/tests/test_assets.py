adf0cea4db92f45b5daac8e1a9ba1a84fba8deb8
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-05-27 11:44:21 +0530

    Create test_assets.py

---
+++ sql/sql/tests/test_assets.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_home_api():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json()["message"] == "Enterprise Asset Management API is running"
+
+def test_register_user():
+    response = client.post(
+        "/register",
+        json={
+            "username": "admin_test",
+            "password": "admin123",
+            "role": "Admin"
+        }
+    )
+
+    assert response.status_code in [200, 400]
